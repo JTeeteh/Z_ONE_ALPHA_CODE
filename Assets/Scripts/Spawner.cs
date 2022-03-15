1773037f512c398fb16dead3397ecaeba9737121
JTor355 <77556051+JTor355@users.noreply.github.com>
2022-03-15 23:13:00 +0800

    updated

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyprefab;
+    public Transform spawnloc;
+    public List<GameObject> enemyList = new List<GameObject>();
+    [SerializeField]
+    float spawndelay;
+    [SerializeField]
+    float spawnlimit;
+    float currentTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            spawnz();
+    }
+
+    public void spawnz()
+    {
+        if (enemyList.Count < spawnlimit)
+        { 
+        
+            currentTime += Time.deltaTime;
+            if (currentTime > spawndelay)
+            {
+            spawndelay += currentTime;
+            enemyList.Add(Instantiate(enemyprefab, spawnloc.position, Quaternion.identity));
+            spawndelay -= currentTime;
+            currentTime = 0.0f;
+            }
+        }
+    }
+}
+
+
+
